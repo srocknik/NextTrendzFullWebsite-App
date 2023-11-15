66192d023d9fd06733e64a9e03abae9ebe6f25b6
srocknik <somesh.chutel90@gmail.com>
2023-11-15 09:59:05 +0000

    NextTrendz-App

---
+++ src/components/Cart/index.js
@@ -0,0 +1,47 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+import CartSummary from '../CartSummary'
+
+import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAllCartItems} = value
+      const showEmptyView = cartList.length === 0
+      const onClickRemoveBtn = () => {
+        removeAllCartItems()
+      }
+
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <div className="my-cart-remove-all-container">
+                  <h1 className="cart-heading">My Cart</h1>
+                  <button
+                    className="remove-all-button"
+                    type="button"
+                    onClick={onClickRemoveBtn}
+                  >
+                    Remove All
+                  </button>
+                </div>
+                <CartListView />
+                <CartSummary />
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
